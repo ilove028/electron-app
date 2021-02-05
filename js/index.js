76ac8177e3d9e452f8e0befa78586cf0ebbc27a2
ilove028 <maoxuheng@cnpc.com.cn>
2021-02-05 17:16:36 +0800

    feat: add project

--- js/index.js
+++ js/index.js
@@ -3,9 +3,11 @@
   const gl = canvas.getContext('webgl');
   const VS_SOURCE = `
     attribute vec2 aPosition;
+    uniform mat3 uRotateMatrix;
+    uniform mat3 uProjectMatrix;
 
     void main() {
-      gl_Position = vec4(aPosition, 0, 1);
+      gl_Position = vec4(vec3(aPosition, 1) * uRotateMatrix * uProjectMatrix, 1);
     }
   `;
   const FS_SOURCE = `
@@ -30,15 +32,22 @@
     reader.addEventListener('loadend', () => {
       // console.log(reader.result);
       const buffer = gl.createBuffer();
+      let pre;
       gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(reader.result), gl.STATIC_DRAW);
-      render(buffer);
+      function tick(current) {
+        pre = pre || current;
+        render(buffer, (current - pre) / 1000 * 180);
+        requestAnimationFrame(tick);
+      }
+
+      tick()
     });
     // reader.readAsText(e.target.files[0]);
     reader.readAsArrayBuffer(e.target.files[0]);
   });
 
-  function render(buffer) {
+  function render(buffer, degree) {
     gl.clearColor(1, 1, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.useProgram(program);
@@ -46,6 +55,12 @@
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
+
+    const uRotateMatrix = gl.getUniformLocation(program, 'uRotateMatrix');
+    gl.uniformMatrix3fv(uRotateMatrix, false, rotate2D(degree));
+
+    const uProjectMatrix = gl.getUniformLocation(program, 'uProjectMatrix');
+    gl.uniformMatrix3fv(uProjectMatrix, false, project(0, canvas.clientWidth, 0, canvas.clientHeight));
 
     gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
@@ -55,6 +70,9 @@
     context.shaderSource(shader, source);
     context.compileShader(shader);
     // TODO compile check
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.log(gl.getShaderInfoLog(shader));
+    }
     return shader;
   }
 
@@ -65,5 +83,28 @@
     context.linkProgram(program);
     // program check
     return program;
+  }
+
+  function createMatrix3Identity() {
+    return [
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1
+    ];
+  }
+
+  function rotate2D(degree) {
+    const radian = degree / 180 * Math.PI;
+    const cosine = Math.cos(radian);
+    const sine = Math.sin(radian);
+    return [cosine, -sine, 0, sine, cosine, 0, 0, 0, 1];
+  }
+
+  function project(left, right, top, bottom) {
+    return [
+      2 / (right - left), 0 , -1,
+      0, 2 / (bottom - top), -1,
+      0, 0, 1
+    ];
   }
 })();
