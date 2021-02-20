12f6c5e86f7c4cf5d3d7a95ac9e5924fb31b1cc5
ilove028 <maoxuheng@cnpc.com.cn>
2021-02-20 14:44:20 +0800

    fix: fix img path

--- js/index.js
+++ js/index.js
@@ -49,6 +49,7 @@
       let pre;
       gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(reader.result), gl.STATIC_DRAW);
+      // handleImage(new Uint8Array(reader.result));
       function tick(current) {
         pre = pre || current;
         render(buffer, (current - pre) / 1000 * 180);
@@ -190,17 +191,21 @@
   new MVideo('video');
 
   function loadTexture() {
-    return loadImage('/img/result.png')
-      .then((img) => {
-        const texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-        textureFlag = 1;
-      })
+    return loadImage('img/result.png')
+      .then(handleImage)
       .catch(console.error);
   }
+
+  function handleImage(img) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+    textureFlag = 1;
+  }
+
+  loadTexture();
 })();
